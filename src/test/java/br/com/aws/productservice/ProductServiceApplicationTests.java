14c16714c203a58626d22284f3d20e9faf76a3d1
Thiago Alvares <thiago.cesaralvares@gmail.com>
2022-12-12 19:40:53 -0300

    Criação e projeto base do serviço de Produtos - AWS

---
+++ src/test/java/br/com/aws/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.aws.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
